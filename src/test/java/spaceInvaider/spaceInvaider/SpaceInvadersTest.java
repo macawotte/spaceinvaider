352eb6dd7de4c531d4459fd2d335591cfe1b8c05
macawotte <maxime.pichon87@outlook.fr>
2018-05-14 14:08:56 +0200

    refactor initialisation

--- src/test/java/spaceInvaider/spaceInvaider/SpaceInvadersTest.java
+++ src/test/java/spaceInvaider/spaceInvaider/SpaceInvadersTest.java
@@ -4,13 +4,24 @@ package spaceInvaider.spaceInvaider;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SpaceInvadersTest {
+	
+	
+	private SpaceInvaders spaceinvaders;
+
+	@Before
+	public void initialisation() {
+		spaceinvaders = new SpaceInvaders(15, 10);
+	}
+	    
+	    
 
 	@Test
 	public void test_AuDebut_JeuSpaceInvaderEstVide() {
-		    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		   
 		    assertEquals("" + 
 		    "...............\n" + 
 		    "...............\n" +
@@ -26,7 +37,7 @@ public class SpaceInvadersTest {
 	
 	@Test
 	public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
-		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+
 		spaceinvaders.positionnerUnNouveauVaisseau(9,7);
 		assertEquals("" + 
 		"...............\n" + 
@@ -43,16 +54,10 @@ public class SpaceInvadersTest {
 	
 	
 	
-	@Test(expected = HorsEspaceJeuException.class)
-	 
-	public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
-		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
-		spaceinvaders.positionnerUnNouveauVaisseau(15,9);
-	}
 
 	@Test
 	public void test_UnNouveauVaisseauPositionneHorsEspaceJeu_DoitLeverUneException() {
-		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+
 		
 		try {
 			spaceinvaders.positionnerUnNouveauVaisseau(15,9);
