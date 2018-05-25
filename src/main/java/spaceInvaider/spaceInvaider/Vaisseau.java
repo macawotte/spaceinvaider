5a98f1fc521c1f98f30752d8b0822d41fe74eac7
macawotte <maxime.pichon87@outlook.fr>
2018-05-25 15:26:05 +0200

    debut ntregration moteur graphique ( fin fonctionnalité 2)

--- src/main/java/spaceInvaider/spaceInvaider/Vaisseau.java
+++ src/main/java/spaceInvaider/spaceInvaider/Vaisseau.java
@@ -1,55 +1,61 @@
 package spaceInvaider.spaceInvaider;
-
 public class Vaisseau {
 
-	int x;
-	int y;
-	int longueur;
-	int hauteur;
+    private Position origine;
+    private Dimension dimension;
 
-	public Vaisseau(int longueur, int hauteur) {
-		this(longueur, hauteur, 0, 0);
-	}
+    public Vaisseau(int longueur, int hauteur) {
+	    this(longueur, hauteur, 0, 0);
+    }
 
-	public Vaisseau(int longueur, int hauteur, int x, int y) {
-		this.longueur = longueur;
-		this.hauteur = hauteur;
-		this.x = x;
-		this.y = y;
-	}
+   public Vaisseau(int longueur, int hauteur, int x, int y) {
+	   this(new Dimension(longueur, hauteur), new Position(x, y));
+    }
 
-	public boolean occupeLaPosition(int x, int y) {
-		return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+    public Vaisseau(Dimension dimension, Position positionOrigine) {
+	    this.dimension = dimension;
+	    this.origine = positionOrigine;
+    }
 
-	}
+    public boolean occupeLaPosition(int x, int y) {
+	    return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
+    }
 
-	public boolean estOrdonneeCouverte(int y) {
-		return (this.y - this.hauteur + 1 <= y) && (y <= this.y);
-	}
+    private boolean estOrdonneeCouverte(int y) {
+	   return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
+    }
 
-	public boolean estAbscisseCouverte(int x) {
-		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-	}
+    private boolean estAbscisseCouverte(int x) {
+	   return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
+    }
 
-	public int abscisseLaPlusADroite() {
-		return this.x + this.longueur - 1;
-	}
+    private int ordonneeLaPlusBasse() {
+	    return this.origine.ordonnee() - this.dimension.hauteur() + 1;
+    }
 
-	public void seDeplacerVersLaDroite() {
-		this.x = this.x + 1;
-	}
+    private int ordonneeLaPlusHaute() {
+	   return this.origine.ordonnee();
+   }
 
-	public void seDeplacerVersLaGauche() {
-		this.x = this.x - 1;
-	}
+   public int abscisseLaPlusADroite() {
+	   return this.origine.abscisse() + this.dimension.longueur() - 1;
+   }
 
-	public int abscisseLaPlusAGauche() {
-		return this.x;
-	}
+   public int abscisseLaPlusAGauche() {
+	   return this.origine.abscisse();
+   }
 
-	public void positionner(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+   public void seDeplacerVersLaDroite() {
+	   this.origine.changerAbscisse(this.origine.abscisse() + 1);
+   }
+
+   public void seDeplacerVersLaGauche() {
+	   this.origine.changerAbscisse(this.origine.abscisse() - 1);
+   }
+
+   public void positionner(int x, int y) {
+	   this.origine.changerAbscisse(x);
+	   this.origine.changerOrdonnee(y);
+   }
 
 }
