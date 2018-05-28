71852526681da50ce7d0a546d503d918f4363fb4
macawotte <maxime.pichon87@outlook.fr>
2018-05-28 14:03:28 +0200

    fonctionnalité 4 Sprite et refactoring

--- src/main/java/spaceInvaider/spaceInvaider/Vaisseau.java
+++ src/main/java/spaceInvaider/spaceInvaider/Vaisseau.java
@@ -1,70 +1,11 @@
 package spaceInvaider.spaceInvaider;
 
-public class Vaisseau {
-
-	private Position origine;
-	private Dimension dimension;
-	private int vitesse;
+public class Vaisseau extends Sprite {
 
 	
-
-	public Vaisseau(Dimension dimension, Position positionOrigine) {
-		this(dimension, positionOrigine, 1);
-	}
-
 	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-		this.dimension = dimension;
-		this.origine = positionOrigine;
-		this.vitesse = vitesse;
+		super(dimension, positionOrigine, vitesse);
 	}
-
-	public boolean occupeLaPosition(int x, int y) {
-		return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
-	}
-
-	private boolean estOrdonneeCouverte(int y) {
-		return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-	}
-
-	private boolean estAbscisseCouverte(int x) {
-		return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-	}
-
-	public int ordonneeLaPlusBasse() {
-		return this.origine.ordonnee() - this.dimension.hauteur() + 1;
-	}
-
-	int ordonneeLaPlusHaute() {
-		return this.origine.ordonnee();
-	}
-
-	public int abscisseLaPlusADroite() {
-		return this.origine.abscisse() + this.dimension.longueur() - 1;
-	}
-
-	public int abscisseLaPlusAGauche() {
-		return this.origine.abscisse();
-	}
-
-	public void seDeplacerVersLaDroite() {
-		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	}
-
-	public void seDeplacerVersLaGauche() {
-		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	}
-
-	public void positionner(int x, int y) {
-		this.origine.changerAbscisse(x);
-		this.origine.changerOrdonnee(y);
-	}
-
-	public int hauteur() {
-		return this.dimension.hauteur();
-	}
-
-	public int longueur() {
-		return this.dimension.longueur();
-	}
+}
 
 }
